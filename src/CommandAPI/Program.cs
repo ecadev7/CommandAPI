2113597bf926f42052ea108d3659adb5ba0b2fc7
emerson cadena <emersondcn@hotmail.com>
2023-02-05 22:18:39 -0500

    added CommandContext to solution

--- src/CommandAPI/Program.cs
+++ src/CommandAPI/Program.cs
@@ -1,10 +1,17 @@
 using CommandAPI.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<CommandContext>(opt =>
+    {
+        opt.UseSqlite(builder.Configuration.GetConnectionString("sqliteConnection"));
+    });
+
 builder.Services.AddControllers();
 
-builder.Services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
+// builder.Services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
+builder.Services.AddScoped<ICommandAPIRepo, SqlCommandAPIRepo>();
 
 var app = builder.Build();
 
